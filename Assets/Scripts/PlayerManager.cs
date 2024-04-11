09f62ead123c958b786e37c2904e3921424397f5
steph1saurus <162734991+steph1saurus@users.noreply.github.com>
2024-04-11 11:17:19 -0400

    initial player script
    transform position, encapsulate speed variable

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    [SerializeField] float minX = 4f;
+    [SerializeField] float maxX = 10f;
+    [SerializeField] float horizontalInput;
+    private float m_speed = 3f;
+    [SerializeField] float speed
+    {
+        get { return m_speed; }
+        set { m_speed = value;}
+
+    } //set one speed that people can't adjust
+  
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,19 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x <minX)
+        {
+            transform.position = new Vector3(minX, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > maxX)
+        {
+            transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+
         
     }
 }
