a67d3896dc0e403cda4e6ead5204ffef1f4c9eeb
steph1saurus <162734991+steph1saurus@users.noreply.github.com>
2024-04-11 10:51:00 -0400

    Main menu functionality
    Start button, choose character, quit

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager instance;
+    [SerializeField] GameObject charMenu;
+
+    private void Awake()
+    {
+        if (instance !=null)
+        {
+            Destroy(gameObject);
+            
+        }
+        DontDestroyOnLoad(gameObject);
+        instance = this;
+        return;
+    }
+  
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 1);   
+    }
+
+    public void QuitGame()
+    {
+        if (Application.isEditor)
+        {
+            EditorApplication.ExitPlaymode();
+        }
+        else
+            Application.Quit();
+    }
+
+    
+    public void CharMenuClicked()
+    {
+        charMenu.SetActive(true);
+    }
+
+    public void CloseButtonClicked()
+    {
+        charMenu.SetActive(false);
+    }
+
+
+
+}
