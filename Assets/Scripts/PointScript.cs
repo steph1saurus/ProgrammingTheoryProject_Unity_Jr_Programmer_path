24982b71446cb599ee03e525771914de70901b67
steph1saurus <162734991+steph1saurus@users.noreply.github.com>
2024-04-18 10:57:38 -0400

    updates to point script and timer script

--- Assets/Scripts/PointScript.cs
+++ Assets/Scripts/PointScript.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PointScript : MonoBehaviour
 {
+    [SerializeField] int point = 1;
+    [SerializeField] int score;
+    [SerializeField] TextMeshProUGUI scoreText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+  
+    private void OnTriggerEnter(Collider other)
     {
-        
+        UpdateScore();   
+    }
+
+    void UpdateScore()
+    {
+        score += point;
+        scoreText.text = "Score: " + score;
     }
 }
--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -1,18 +1,65 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TimerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float currentTime = 90f;
+    [SerializeField] TextMeshProUGUI countdownText;
+    [SerializeField] bool gameActive;
+
     void Start()
     {
-        
+        StartCoroutine(Countdown(3));
+        gameActive = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Countdown(int seconds)
     {
-        
+        int count = seconds;
+
+        while (count > 0)
+        {
+            countdownText.text = count.ToString();
+            yield return new WaitForSeconds(1);
+            count--;
+        }
+
+        countdownText.text = "Go!";
+        yield return new WaitForSeconds(1);
+
+        countdownText.gameObject.SetActive(false); // Hide the countdown text after countdown finishes
+        gameActive = true;
+    }
+
+    private void Update()
+    {
+        if (gameActive)
+        {
+            StartTimer();
+        }
+    }
+
+    void StartTimer()
+    {
+        currentTime -= Time.deltaTime;
+        SetTimerText();
+    }
+
+    private void SetTimerText()
+    {
+        timerText.text = currentTime.ToString("0.0"); // Round the time to nearest integer
+
+        if (currentTime <= 10)
+        {
+            timerText.color = Color.red;
+        }
+
+        if (currentTime <= 0)
+        {
+            timerText.text = "0.0";
+            enabled = false;
+        }
     }
 }
